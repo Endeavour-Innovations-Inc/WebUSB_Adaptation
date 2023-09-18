770069bba227b7c07c59fd9ccf597216754ed372
Endeavour-1 <50341461+Endeavour-Innovations-Inc@users.noreply.github.com>
2023-09-18 16:11:49 -0700

    Update usb_reader.py
    Updated some code to support the Black Magic connection through the UART interface

--- usb_reader.py
+++ usb_reader.py
@@ -1,5 +1,6 @@
 import usb.core
 import usb.util
+import serial
 
 def simulate_usb_data(data_size=64):
     # Simulate data from a device
@@ -28,13 +29,39 @@ def get_usb_data(vendor_id, product_id, endpoint, data_size=64, simulate=False):
     
     return data.tobytes()  # convert from array to bytes
 
+def get_uart_data(com_port, baud_rate=115200, data_size=64, simulate=False):
+    # Adjusted baud rate for BMP; it often operates at 115200, but you might need to confirm
+    if simulate:
+        return simulate_usb_data(data_size)
+
+    # Open the COM port
+    with serial.Serial(com_port, baud_rate, timeout=1) as ser:
+        data = ser.read(data_size)
+        
+    return data
+
 def main():
-    # Adjust these values to your device
-    VENDOR_ID = 0x1234
-    PRODUCT_ID = 0x5678
-    ENDPOINT_ADDRESS = 0x81  # usually 0x81 or similar for IN endpoint
+    choice = input("Select communication type (USB/UART/BMP): ").upper()
     
-    data = get_usb_data(VENDOR_ID, PRODUCT_ID, ENDPOINT_ADDRESS, simulate=True)
+    if choice == "USB":
+        # Adjust these values to your device
+        VENDOR_ID = 0x1234
+        PRODUCT_ID = 0x5678
+        ENDPOINT_ADDRESS = 0x81  # usually 0x81 or similar for IN endpoint
+        data = get_usb_data(VENDOR_ID, PRODUCT_ID, ENDPOINT_ADDRESS, simulate=True)
+
+    elif choice == "UART":
+        COM_PORT = 'COM3'  # Adjust this value to your COM port
+        BAUD_RATE = 9600   # Baud rate, adjust as per your setup
+        data = get_uart_data(COM_PORT, BAUD_RATE, simulate=True)
+
+    elif choice == "BMP":
+        BMP_COM_PORT = 'COM4'  # This is an example. You'll need to adjust based on which port BMP UART passthrough is on.
+        data = get_uart_data(BMP_COM_PORT, simulate=True)
+        
+    else:
+        print("Invalid choice!")
+        return
 
     # Write the data to a txt file
     with open("data.txt", "wb") as file:
