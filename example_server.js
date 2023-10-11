7ea78a85b9728efe8c540b44a54efc3d8546efbc
Endeavour-1 <50341461+Endeavour-Innovations-Inc@users.noreply.github.com>
2023-10-11 09:47:41 -0700

    Added js files and sample data

---
+++ example_server.js
@@ -0,0 +1,22 @@
+const PORT = 8000;
+
+var express = require('express');
+var app = express();
+
+const cors = require("cors");
+app.use(cors({
+    origin: 'null'
+}));
+
+app.use(express.static('.'));
+
+//app.use('.', express.static(__dirname + '.'));
+
+app.get('.', function(req, res) {
+    res.sendFile(path.join(public, '.'));
+});
+
+var server = app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
+
+//console.log('Listening on port 80');
+
---
+++ plotter.js
@@ -0,0 +1,133 @@
+
+  armProgram();
+
+  //****************************** csv file handling ******************************//
+  //declare two arrays to store time and sine values
+  
+  const time_data = []; //global time variable
+  const point_data = []; //global data variable
+  var dataFile;
+
+  makePlotly(time_data, point_data);
+    
+  async function armProgram()
+  {
+    //resets canvas
+    const reset = document.getElementById('reset').
+      addEventListener('click', () => {
+        
+        //calls localhost:9000 to remove data.csv from root directory
+        //fetch('http://localhost:9000/data.csv');
+
+        time_data.length = 0; //global time variable
+        point_data.length = 0; //global data variable
+        makePlotly(time_data, point_data);
+    });
+
+    //calls localhost:8000 to load data.csv, parses file, draws canvas
+    const upload = document.getElementById('arm').
+      addEventListener('click', () => {
+
+      fetch('http://localhost:9000/data.csv');
+      time_data.length = 0; //global time variable
+      point_data.length = 0; //global data variable
+      makePlotly(time_data, point_data);
+
+      Papa.parse('http://localhost:8000/data.csv',
+      {
+        download: true,
+        header: true,
+        skipEmptyLines: true,
+        complete: function(results)
+        {
+            //console.log(results); //<===== for testing
+            //console.log(results.data[0].sine); //<===== for testing
+
+            time_data.length = 0;     //clear out time array
+            point_data.length = 0;    //clear out data_point array
+            //push all values into their own array
+            for(i = 0; i < results.data.length; i++)
+            {
+              time_data.push(results.data[i].t);      //push time values into time_data[]
+              point_data.push(results.data[i].data);  //push point values into point_data[]
+            }
+            //console.log(time_data); //<===== for testing
+            //console.log(point_data); //<===== for testing
+            makePlotly(time_data, point_data);
+        }
+      });
+    });
+  } 
+  // console.log(time); //<===== for testing
+  // console.log(sine); //<===== for testing
+
+  //****************************** Plotly.js Graph Control ******************************//
+  //data graphing control
+   function makePlotly(time_data, point_data)
+  {
+    myChart = document.getElementById('chart');
+    Plotly.newPlot(myChart, data, layout, config);
+    // Plotly.newPlot( CHART, layout,[{
+    //   x: time_data,   //x-axis data
+    //   y: point_data,  //y-axis datas
+    //   type:'line'     //chart type
+    // }]);
+  }
+
+/*  async function getData(){
+    if('http://localhost:8000/data.csv'.exists()){
+      let dataFile = await fetch('http://localhost:8000/data.csv');
+      return;
+    }else getData();
+  }
+*/
+  //automatically resize to fit device's window size
+  var config = {responsive: true}
+
+  //first data trace 
+  var trace1 = {
+    x: time_data,   //x-axis data
+    y: point_data,  //y-axis datas
+    //mode:'markers + line',
+    type:'line'     //chart type
+  }
+
+  //data to be ploted to chart (for furture ploting multiple data on the same graph)
+  var data = [trace1];
+
+  //plotly chart customization
+  var layout = {
+    autosize: true,
+    // width: 1500,
+    // height: 680,
+    title:{
+      text: 'Input Signal', //chart title
+      font:{
+        family: 'Arial',    //title's font type
+        size: 25            //title's font size
+      }
+    },
+
+    xaxis:{
+      //automargin: true,
+      title:{
+        text: 'Time', //chart title
+        font:{
+          family: 'Arial',    //title's font type
+          size: 20            //title's font size
+        }
+      }
+    },
+
+    yaxis:{
+      //automargin: true,
+      title:{
+        text: 'Amplitude (V)', //chart title
+        font:{
+          family: 'Arial',    //title's font type
+          size: 20            //title's font size
+        }
+      }
+    },
+  };
+ 
