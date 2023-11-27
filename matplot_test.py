1abcb410b0d8c86ceee0e41818646c90f14c1cb9
garyfishel <gary.fishel@wsu.edu>
2023-11-27 13:57:06 -0800

    updated usb backend

--- matplot_test.py
+++ matplot_test.py
@@ -12,8 +12,12 @@ scope_interface.connect_to_scope()
 t_data = []
 v_data = []
 
-#while(scope_interface.check_for_data() != 1):
-#    continue
+data_ready = None
+while(data_ready == None):
+    try:
+        data_ready = check_for_data()
+    except usb.core.USBError:
+        pass
 
 v_data = scope_interface.get_samples()
 #print(v_data)
